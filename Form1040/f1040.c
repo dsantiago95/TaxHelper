26be46332666497c9316dce16b18efa020e24b83
dsantiago95 <dialyn.santiago@outlook.com>
2025-03-16 20:39:42 -0400

    Working up f1040 and Schedule1.

--- Form1040/f1040.c
+++ Form1040/f1040.c
@@ -31,6 +31,8 @@ struct TaxPayer {
 
 void initialize_taxpayer(struct TaxPayer *taxpayer);
 
+void add_wage_income(struct TaxPayer *taxpayer, int wages);
+
 struct Dependent {
 	char name[35];
 	struct Date dob;
@@ -46,6 +48,7 @@ struct f1040 {
 	struct Date filing_date;
 	struct TaxPayer *primary_taxpayer;
 	struct TaxPayer *spouse;
+	int status;
 	struct Address *address;
 	struct Dependent *dependents[MAX_DEP];
 	int w2_wages;
@@ -77,13 +80,13 @@ struct Schedule1 {
 	int business_income; //negative if loss
 	int rental_income;
 	int unemployment_comp;
-	//a catch-all member until other tax types are implemented
-	int other_additional_income; 
+	int additional_income;  // 2024TY: line 10
 
 	int se_tax_deduction;
-	//a catch-all member until other deduction types are implemented
-	int other_deductions;
+	int adjustments_to_income; // 2024TY: line 26
 };
+
+void initialize_schedule1(struct Schedule1 *schedule);
 
 struct Schedule2 {
 	int excess_ptc;
@@ -111,12 +114,20 @@ void add_dependent(struct f1040 *tax_return, struct Dependent dependent);
 
 void add_address(struct f1040 *tax_return, struct Address address);
 
+/*******\
+1 = single
+2 = MFJ
+3 = MFS
+4 = HoH
+/*******/
+void set_status(struct f1040 *tax_return, int status);
+
 //sums income from Schedule 1 and adds to f1040 struct.
 void calculate_total_income(struct f1040 *tax_return, struct Schedule1 schedule);
 
 void calculate_agi(struct f1040 *tax_return, struct Schedule1 schedule);
 
-void set_std_deduction(struct f1040 *tax_return, int status);
+void set_std_deduction(struct f1040 *tax_return);
 
 //Form 8995
 void calculate_qbi(struct f1040 *tax_return);
@@ -156,7 +167,13 @@ void initialize_tax_return(struct f1040 *tax_return, int tax_year)
 {
 	tax_return->tax_year = tax_year;
 	tax_return->primary_taxpayer = NULL;
+	tax_return->address = NULL;
 	tax_return->spouse = NULL;
+	tax_return->status = 0;
+	for (int i = 0; i < MAX_DEP; i++)
+	{
+		tax_return->dependents[i] = NULL;
+	}
 	tax_return->w2_wages = 0;
 	tax_return->sch1_additional_income = 0;
 	tax_return->total_income = 0;
@@ -179,6 +196,8 @@ void initialize_tax_return(struct f1040 *tax_return, int tax_year)
 	tax_return->eic_credit = 0;
 	tax_return->actc_credit = 0;
 	tax_return->aotc_credit = 0;
+
+	printf("Tax Return initialized successfuly.\n");
 }
 
 void initialize_dependent(struct Dependent *dep)
@@ -186,6 +205,8 @@ void initialize_dependent(struct Dependent *dep)
 	dep->ctc_qualify = false;
 	dep->odc_qualify = false;
 	dep->eic_qualify = false;
+
+	printf("Dependent initialized successfuly.\n");
 }
 
 void initialize_taxpayer(struct TaxPayer *taxpayer)
@@ -194,6 +215,27 @@ void initialize_taxpayer(struct TaxPayer *taxpayer)
 	taxpayer->rental_income = 0;
 	taxpayer->se_income = 0; //Sch C, Sch E pg2
 	taxpayer->eic_qualify = false;
+
+	printf("Taxpayer initialized successfuly.\n");
+}
+
+void initialize_schedule1(struct Schedule1 *schedule)
+{
+
+	schedule->business_income = 0; //negative if loss
+	schedule->rental_income = 0;
+	schedule->unemployment_comp = 0;
+	schedule->additional_income = 0;  // 2024TY: line 10
+
+	schedule->se_tax_deduction = 0;
+	schedule->adjustments_to_income = 0; // 2024TY: line 26
+	printf("Schedule 1 initialized.\n");
+}
+
+void add_wage_income(struct TaxPayer *taxpayer, int wages)
+{
+	taxpayer->w2_wages += wages;
+	printf("Wages of %d added.\n", taxpayer->w2_wages);
 }
 
 void add_taxpayer(struct f1040 *tax_return, struct TaxPayer taxpayer)
@@ -201,16 +243,35 @@ void add_taxpayer(struct f1040 *tax_return, struct TaxPayer taxpayer)
 	if (tax_return->primary_taxpayer == NULL)
 	{
 		tax_return->primary_taxpayer = &taxpayer;
+		printf("Taxpayer added successfuly.\n");
 		return;
 	} else if (tax_return->primary_taxpayer != NULL & tax_return->spouse == NULL)
 	{
 		tax_return->spouse = &taxpayer;
+		printf("Spouse added successfuly.\n");
 		return;
 	}
 	printf("Already 2 taxpayers on the return.\n");
 	return;
 }
 
+void set_status(struct f1040 *tax_return, int status)
+{
+	if (status == 1 | status == 4)
+	{
+		if (tax_return->spouse == NULL)
+		{
+			tax_return->status = status;
+			printf("Status set successfuly.\n");
+			return;
+		}
+		printf("Cannot set status to single or head of household if spouse is present. No changes made.\n");
+		return;
+	}
+	tax_return->status = status;
+	printf("Status set successfuly.\n");
+	return;
+}
 
 struct Address create_address(char street[35], char city[35], char state[2], char zip[5])
 {
@@ -219,6 +280,7 @@ struct Address create_address(char street[35], char city[35], char state[2], cha
 	strcpy(return_add.city, city);
 	strcpy(return_add.state, state);
 	strcpy(return_add.zip_code, zip);
+	printf("New address created.\n");
 	return return_add;
 }
 
@@ -227,39 +289,63 @@ void add_address(struct f1040 *tax_return, struct Address address)
 	if (tax_return->address == NULL)
 	{
 		tax_return->address = &address;
+		printf("Address added to return.\n");
 		return;
 	} 
-	printf("Address already assigned. No change.");
+	printf("Address already assigned. No change.\n");
 	return;
 }
 
 void add_dependent(struct f1040 *tax_return, struct Dependent dependent)
 {
-	for (int i = 0; i < MAX_DEP; i++;)
+	for (int i = 0; i < MAX_DEP; i++)
 	{
 		if (tax_return->dependents[i] != NULL)
 		{
-			tax_return->dependents[i] = dependent;
+			tax_return->dependents[i] = &dependent;
+			printf("New dependent added.\n");
 			return;
 		}
 	}
 	printf("Max number of dependents already present. No changes\n");
 	return;
 }
+
+void calculate_total_income(struct f1040 *tax_return, struct Schedule1 schedule)
+{
+	tax_return->w2_wages += tax_return->primary_taxpayer->w2_wages;
+	if (tax_return->status == 2)
+	{
+		tax_return->w2_wages += tax_return->spouse->w2_wages;
+	}
+	tax_return->total_income += tax_return->w2_wages;
+	tax_return->total_income += schedule.adjustments_to_income;
+	printf("Total income calculated: %d.\n", tax_return->total_income);
+}
+
 int main()
 {
 	struct f1040 tax_return;
 	initialize_tax_return(&tax_return, 2024);
+	
+	struct Schedule1 sch1;
+	initialize_schedule1(&sch1);
 
 	struct TaxPayer primary;
 	initialize_taxpayer(&primary);
 	add_taxpayer(&tax_return, primary);
+
+	set_status(&tax_return, 1);
 
 	struct Dependent dependent;
 	initialize_dependent(&dependent);
 
 	struct Address address = create_address("831 Elizabeth Ave", "Elizabeth", "NJ", "07201");
 	add_address(&tax_return, address);
+
+	add_wage_income(&primary, 40000);
+
+	calculate_total_income(&tax_return, sch1);
 
 	printf("compiled!\n");
 }
